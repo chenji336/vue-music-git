5c8ac7a56a605e9cf4ee8ffa83a36429f54d3ba6
chenji336 <1010072738@qq.com>
2018-02-27 23:56:02 +0800

    player-1

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 	import MHeader from 'components/m-header/m-header'
 	import Tab from 'components/tab/tab'
+  import Player from 'components/player/player'
 	export default {
     // created() {
     //   this.$http.get('http://t.yushu.im/v2/movie/in_theaters?start=0&count=3').then((response) => {
@@ -19,7 +21,8 @@
     // },
 		components: {
 			MHeader,
-			Tab
+			Tab,
+      Player
 		}
 	}
 </script>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -23,7 +23,7 @@
       ref='list'
     >
       <div class='song-list-wrapper'>
-        <song-list :songs='songs' ></song-list>
+        <song-list :songs='songs' @select='selectItem' ></song-list>
       </div>
       <div class='loading-container' v-show='!songs.length'>
         <loading></loading>
@@ -37,6 +37,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -87,7 +88,14 @@
       },
       back() {
         this.$router.back()
-      }
+      },
+      selectItem(item, index) {
+         this.selectPlay({
+          list: this.songs,
+          index
+         })
+      },
+      ...mapActions(['selectPlay'])
     },
     watch: {
       scrollY(newY) {
